3542896f2dafebe338015f9d24ba8941afccb63b
Abdullah9110 <srede212077@gmail.com>
2023-06-21 12:12:03 +0300

    different sign up options

--- app/src/main/java/com/example/trainingcenter/View/SignUpActivity.java
+++ app/src/main/java/com/example/trainingcenter/View/SignUpActivity.java
@@ -1,30 +1,19 @@
 package com.example.trainingcenter.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.trainingcenter.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
-
-    private FirebaseAuth auth;
-    private EditText signupEmail, signupPassword;
-    private Button signupButton;
+    private Button btnInstructor;
+    private Button btnTrainee;
+    private Button btnAdmin;
     private TextView loginRedirectText;
 
     @Override
@@ -32,42 +21,30 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        auth = FirebaseAuth.getInstance();
-        signupEmail = findViewById(R.id.signup_email);
-        signupPassword = findViewById(R.id.signup_password);
-        signupButton = findViewById(R.id.signup_button);
-        loginRedirectText = findViewById(R.id.loginRedirectText);
+        btnInstructor = findViewById(R.id.btnInstructor);
+        btnAdmin = findViewById(R.id.btnAdmin);
+        btnTrainee = findViewById(R.id.btnTrainee);
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        loginRedirectText = findViewById(R.id.loginRedirectText_admin);
+
+        btnInstructor.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String user = signupEmail.getText().toString().trim();
-                String pass = signupPassword.getText().toString().trim();
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, SignUp_Instructor_Activity.class));
+            }
+        });
 
-                if (user.isEmpty()){
-                    signupEmail.setError("Email cannot be empty");
-                }
-                else if (pass.isEmpty()){
-                    signupPassword.setError("Password cannot be empty");
-                }
-                else if (!validatePassword(pass)){
-                    signupPassword.setError("Invalid Password\nMinimum 8 characters and maximum 15 characters\n" +
-                            "It must contain at least one number, one lowercase letter, and one uppercase letter.");
-                }
-                else{
-                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
-                            } else {
-                                Toast.makeText(SignUpActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, SignUp_Admin_Activity.class));
+            }
+        });
 
+        btnTrainee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, SignUp_Trainee_Activity.class));
             }
         });
 
@@ -78,17 +55,5 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
-    }
-    public static boolean validatePassword(String pass) {
-        // Minimum 8 characters and maximum 15 characters
-        if (pass.length() < 8 || pass.length() > 15) {
-            return false;
-        }
-
-        // At least one number, one lowercase letter, and one uppercase letter
-        Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).+$");
-        Matcher matcher = pattern.matcher(pass);
-        System.out.println(matcher.matches());
-        return matcher.matches();
     }
 }
---
+++ app/src/main/java/com/example/trainingcenter/View/SignUp_Admin_Activity.java
@@ -0,0 +1,94 @@
+package com.example.trainingcenter.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.trainingcenter.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SignUp_Admin_Activity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword;
+    private Button signupButton;
+    private TextView loginRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_admin);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email_admin);
+        signupPassword = findViewById(R.id.signup_password_admin);
+        signupButton = findViewById(R.id.signup_button_admin);
+        loginRedirectText = findViewById(R.id.loginRedirectText_admin);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+
+                if (user.isEmpty()){
+                    signupEmail.setError("Email cannot be empty");
+                }
+                else if (pass.isEmpty()){
+                    signupPassword.setError("Password cannot be empty");
+                }
+                else if (!validatePassword(pass)){
+                    signupPassword.setError("Invalid Password\nMinimum 8 characters and maximum 15 characters\n" +
+                            "It must contain at least one number, one lowercase letter, and one uppercase letter.");
+                }
+                else{
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUp_Admin_Activity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUp_Admin_Activity.this, LoginActivity.class));
+                            } else {
+                                Toast.makeText(SignUp_Admin_Activity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUp_Admin_Activity.this, LoginActivity.class));
+            }
+        });
+
+    }
+    public static boolean validatePassword(String pass) {
+        // Minimum 8 characters and maximum 15 characters
+        if (pass.length() < 8 || pass.length() > 15) {
+            return false;
+        }
+
+        // At least one number, one lowercase letter, and one uppercase letter
+        Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).+$");
+        Matcher matcher = pattern.matcher(pass);
+        System.out.println(matcher.matches());
+        return matcher.matches();
+    }
+}
